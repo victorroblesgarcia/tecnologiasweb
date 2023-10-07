ed3303eba394ffa8d047fcced0f11f3c8e845e9e
victorroblesgarcia <victor.roblesgarcia96@gmail.com>
2023-10-07 01:33:05 -0600

    Practica 2

---
+++ p02/inclusion_archivos/cuerpo.inc.php
@@ -0,0 +1,4 @@
+<?php
+echo"<h1>Esteeselcuerpodeldocumento</h1>";
+echo"<h2>Esteeselcuerpodeldocumento</h2>";
+?>
---
+++ p02/inclusion_archivos/pie.inc.php
@@ -0,0 +1,10 @@
+<hr/>
+<?php
+echo"<div><h1style=\"border-width:3;border-style:groove;background-color:
+#ffcc99;\">FinaldelapáginaPHPVínculosútiles:<ahref=\"php.net\">php.net</a>
+&nbsp;<ahref=\"mysql.org\">mysql.org</a></h1>";
+echo"Nombredelarchivoejecutado:",$_SERVER['PHP_SELF'],"&nbsp;&nbsp;&nbsp;";
+echo"Nombredelarchivoincluido:",__FILE__,"</div>";
+?>
+</body>
+</html>
